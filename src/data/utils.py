711a4504fecc7be8e1156a9fe96bd36d703b9b3d
rociotatiana <rtpereza@gmail.com>
2025-11-27 15:41:44 -0300

    notebook: randomforest con stratify

--- src/data/utils.py
+++ src/data/utils.py
@@ -9,9 +9,16 @@ def cat_p_factor(df):
         labels=[0, 1, 2]
     ).astype(int)
 
-def cat_p_factor_extreme(df):
-    return pd.cut(
-        df["p_factor"],
-        bins=[-np.inf, 0.946, np.inf],
-        labels=[0, 1]
-    ).astype(int)
+def bin_p_factor(df, bool):
+    if bool==True:
+        return pd.cut(
+            df["p_factor"],
+            bins=[-np.inf, 0.946, np.inf],
+            labels=[0, 1]
+        ).astype(int)
+    else:
+        return pd.cut(
+            df["p_factor"],
+            bins=[-np.inf, 0.44, np.inf],
+            labels=[0, 1]
+        ).astype(int)
